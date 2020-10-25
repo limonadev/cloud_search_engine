87cd5324dcb351b5a28aadddc494c5b635f7288e
limonadev <limonadev@whiteleaf.dev>
2020-10-25 13:38:52 -0500

    Added script to retrieve page rank results

--- Cloud/RequestTests/run.sh
+++ Cloud/RequestTests/run.sh
@@ -1,2 +1,3 @@
 export GOOGLE_APPLICATION_CREDENTIALS="My Project-3778cd48121b.json"
+mkdir -p Rank/output
 python storage_getter.py
--- Cloud/RequestTests/storage_getter.py
+++ Cloud/RequestTests/storage_getter.py
@@ -4,4 +4,8 @@ storage_client = storage.Client()
 
 print('nani')
 bucket = storage_client.get_bucket('test_bucket_limonadev')
-print(bucket)
+for blob in bucket.list_blobs():
+    if blob.name[:7] == 'output/' and blob.name != 'output/':
+        with open(f'Rank/{blob.name}', "wb") as f:
+            blob.download_to_file(f)
+            print(blob.name + ' ' + '#'*70)
