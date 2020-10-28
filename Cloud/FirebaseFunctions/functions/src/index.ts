de58c742f647b89b84c716159277966454539c98
limonadev <limonadev@whiteleaf.dev>
2020-10-28 17:33:06 -0500

    Handling post parameters

--- Cloud/FirebaseFunctions/functions/src/index.ts
+++ Cloud/FirebaseFunctions/functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 
-//var admin = require('firebase-admin');
-//var db = admin.database();
+// var admin = require('firebase-admin');
+// var db = admin.database();
 
 //https://us-central1-prismatic-vial-174715.cloudfunctions.net/helloWorld
 export const helloWorld = functions.https.onRequest((request, response) => {
@@ -9,12 +9,26 @@ export const helloWorld = functions.https.onRequest((request, response) => {
         response.sendStatus(400).send('You need to do a POST request!');
     }
 
-    //var wordsRef = db.ref('index');
+    functions.logger.info('Nani');
+
+    let words = request.body.words;
+
+    for (let word of words) {
+        functions.logger.info(word);
+    }
+
+    /*functions.logger.info(lol);
+
+    for (var word in lol.words) {
+        functions.logger.info(word);
+    }*/
+
+    /*var wordsRef = db.ref('index/'+request);
     let data = request.body;
 
-    functions.logger.info(JSON.stringify(data));
+    functions.logger.info(JSON.stringify(data));*/
 
-    response.sendStatus(200);
+    response.sendStatus(200).send(words);
 });
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
